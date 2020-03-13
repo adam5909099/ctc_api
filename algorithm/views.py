19fa0b520bf86edc6f803b4873729fef449d6c3c
adam5909099 <adam5909099@gmail.com>
2020-03-12 23:24:56 -0400

    Implement keyword extractor

--- algorithm/views.py
+++ algorithm/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import yake
 
-# Create your views here.
+
+class KeywordExtractor(APIView):
+    def post(self, request):
+        kw_extractor = yake.KeywordExtractor()
+        keywords = kw_extractor.extract_keywords(request.data.get('text'))
+        keywords.sort(key=lambda x: x[1])
+        return Response([keyword[0] for keyword in keywords])
--- ctc_api/settings.py
+++ ctc_api/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'knox',
-    'authentication'
+    'authentication',
+    'algorithm'
 ]
 
 REST_FRAMEWORK = {
