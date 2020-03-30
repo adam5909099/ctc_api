8b93ad2fee058b1145a4d0737f2f15d22b13a6a7
adam5909099 <adam5909099@gmail.com>
2020-03-29 22:38:53 -0400

    Implement resume upload

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, parsers, response, decorators
 from .models import Job, Position
-from .serializers import JobSerializer, PositionSerializer
-from .serializers import PositionSerializer
+from .serializers import JobSerializer, PositionSerializer, JobResumeSerializer
 
 
 class PositionViewSet(viewsets.ModelViewSet):
@@ -12,3 +11,17 @@ class PositionViewSet(viewsets.ModelViewSet):
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    @decorators.action(
+        detail=True,
+        methods=['PUT'],
+        serializer_class=JobResumeSerializer,
+        parser_classes=[parsers.MultiPartParser]
+    )
+    def resume_upload(self, request, pk):
+        obj = self.get_object()
+        serializer = self.serializer_class(
+            obj, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data)
