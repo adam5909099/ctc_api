59e33fd5a7ee8e5814f573098803653361610255
wyin125 <wyin125@protonmail.com>
2020-04-10 22:29:13 -0400

    Implment resume extracting

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Job
+from pdfminer.high_level import extract_text
 
 
 class JobSerializer(serializers.ModelSerializer):
@@ -12,4 +13,8 @@ class JobSerializer(serializers.ModelSerializer):
 class ResumeUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ['resume']
+        fields = ['resume', 'resume_text']
+
+    def update(self, instance, validated_data):
+        instance.resume_text = extract_text(validated_data.get('resume'))
+        return instance
