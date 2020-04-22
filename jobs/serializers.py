e50820cff46e191fe98d46d52684368225869973
adam5909099 <adam5909099@gmail.com>
2020-04-22 01:58:02 -0700

    Calculate resume score

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -12,10 +12,10 @@ class JobSerializer(serializers.ModelSerializer):
     resume_keywords = serializers.SerializerMethodField()
 
     def get_keywords(self, obj):
-        return json.loads(obj.keywords) if obj.keywords else null
+        return json.loads(obj.keywords) if obj.keywords else None
 
     def get_resume_keywords(self, obj):
-        return json.loads(obj.resume_keywords) if obj.resume_keywords else null
+        return json.loads(obj.resume_keywords) if obj.resume_keywords else None
 
     def create(self, validated_data):
         instance = Job.objects.create(**validated_data)
@@ -40,11 +40,18 @@ class ResumeUploadSerializer(serializers.ModelSerializer):
         without_cid = re.sub(r'\(cid:\d*\)', '', printable)
         stripped = re.sub('\n{2,}', '\n\n', without_cid).strip().strip('\n')
         instance.resume_text = stripped
-        instance.resume_keywords = json.dumps(get_keywords(stripped))
+
+        resume_keywords = get_keywords(stripped)
+        instance.resume_keywords = json.dumps(resume_keywords)
+
+        skills = list(map(lambda x: x['skill'], json.loads(instance.keywords)))
+        resume_skills = list(map(lambda x: x['skill'], resume_keywords))
+        matching_count = len([skill for skill in skills if skill in resume_skills])
+        instance.score = matching_count / len(skills) if len(skills) else 0
 
         instance.save()
         return instance
 
     class Meta:
         model = Job
-        fields = ['resume', 'resume_text']
+        fields = ['resume', 'resume_text', 'resume_keywords', 'score']
